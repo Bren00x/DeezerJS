46d22aacd8b26701a7da259865a6ac2ed83db1b5
bren00x <bren4school@gmail.com>
2023-04-18 17:36:49 -0400

    Ditch Typescript cause man idk

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Album from "methods/album";
+import Artist from "methods/artist";
+import Genre from "methods/genre";
+import Playlist from "methods/playlist";
+import Search from "methods/search";
+import Track from "methods/track";
+import User from "methods/user";
